ba3139ff524d897230e20ae8bea68face3503928
gudepeng <gudepeng@rongcapital.cn>
2018-02-05 16:06:40 +0800

    1.0

--- icoding-security/src/main/java/top/icoding/security/controller/NotHasAuthorityController.java
+++ icoding-security/src/main/java/top/icoding/security/controller/NotHasAuthorityController.java
@@ -12,24 +12,30 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
-* @author 我是金角大王
-* @date 2018年1月25日 下午1:38:04
-*/
-public class NotHasAuthorityController extends LoginUrlAuthenticationEntryPoint{  
-	
+ * @author 我是金角大王
+ * @date 2018年1月25日 下午1:38:04
+ */
+public class NotHasAuthorityController extends LoginUrlAuthenticationEntryPoint {
+
 	public NotHasAuthorityController(String loginFormUrl) {
 		super(loginFormUrl);
 	}
 
-	//当访问的资源没有权限，会调用这里  
-    @Override  
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)  
-                throws IOException, ServletException {  
-           //返回json形式的错误信息  
-           response.setCharacterEncoding("UTF-8");  
-           response.setContentType("application/json");  
-           response.setStatus(response.SC_UNAUTHORIZED);
-           response.getWriter().println("{\"status\":1,\"message\":\""+authException.getLocalizedMessage()+"\"}");  
-           response.getWriter().flush();   
-        }  
-}  
+	// 当访问的资源没有权限，会调用这里
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		// 返回json形式的错误信息
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE ,PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,Content-Type");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		response.setStatus(response.SC_UNAUTHORIZED);
+		response.getWriter().println("{\"status\":1,\"message\":\"" + authException.getLocalizedMessage() + "\"}");
+		response.getWriter().flush();
+		response.getWriter().close();
+	}
+}
